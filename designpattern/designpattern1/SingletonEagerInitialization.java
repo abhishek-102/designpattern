8bf99e1165d9ba1461841f0cce96e205c9566b69
abhishek-qa <46737087+abhishek-qa@users.noreply.github.com>
2019-01-31 14:01:03 +0530

    commit

---
+++ designpattern/designpattern1/SingletonEagerInitialization.java
@@ -0,0 +1,14 @@
+package designpattern1;
+
+public class SingletonEagerInitialization {
+
+	public static SingletonEagerInitialization singleObject = new SingletonEagerInitialization();
+	
+	private SingletonEagerInitialization() {	
+	}
+
+	public void checkInstance() {
+
+		System.out.println("Instance is created");
+	}
+}
---
+++ designpattern/designpattern2/Pc.java
@@ -0,0 +1,8 @@
+package designpattern2;
+
+public class Pc implements Computer{
+	public void createComputer() {
+		System.out.println("PC computer is Created");
+	}
+
+}
